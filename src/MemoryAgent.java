222d9c5aa540f0c38909ca99ee5126ef3b360737
elijah.pennoyer <elijah.pennoyer@gmail.com>
2016-11-17 15:44:22 -0700

    main body of MemoryAgent function

--- src/MemoryAgent.java
+++ src/MemoryAgent.java
@@ -1,25 +1,51 @@
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class MemoryAgent extends Thread {
 
 	public WriteBuffer wBuffer;
 	public MainMemory mMemory;
+	private boolean finished = false;
+	static boolean ONE = true;
 	
+	/**
+	 * The constructor for MemoryAgent.
+	 * @param inputWriteBuffer The WriteBuffer for the Processor creating the MemoryAgent
+	 * @param inputMainMemory The MainMemory
+	 */
 	public MemoryAgent(WriteBuffer inputWriteBuffer, MainMemory inputMainMemory){
 		wBuffer = inputWriteBuffer;
 		mMemory = inputMainMemory;
 	}
 	
-	
 	public void run() {
-		boolean ONE = true;
+		//Run loop until endThread is called, then end the thread.
 		while(ONE){
 			
-			//Variable to_store = null;
+			memoryVariable nextVariable = wBuffer.nextVariableToStore();
+			if(nextVariable == null){
+				//if endThread has been called - Setting finished to true - End the thread.
+				if(finished){
+					break;
+				}
+				else{ //Otherwise, restart the loop
+					continue;
+				}
+				
+			}
+			
+			//Store the variable to the MainMemory
+			mMemory.store(nextVariable.vName, nextVariable.value);
 			
 			
 		}
 		
 	}
 	
+	/**
+	 * Tells the thread to end when there are no more elements to store. 
+	 */
+	public void endThread(){
+		finished = true;
+	}
 	
 }
