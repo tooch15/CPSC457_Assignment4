f00e89261102e4892961ef8bc4becacf579dd1c2
elijah.pennoyer <elijah.pennoyer@gmail.com>
2016-11-17 16:16:29 -0700

    Comments in Processor

--- src/Processor.java
+++ src/Processor.java
@@ -1,8 +1,17 @@
 
 public class Processor extends Thread {
 	
+	/**
+	 * The WriteBuffer of the Processor
+	 */
 	public WriteBuffer wBuffer;
+	/**
+	 * The MemoryAgent of the Processor
+	 */
 	public MemoryAgent mAgent;
+	/**
+	 * The MainMemory that the processor uses
+	 */
 	public MainMemory mMemory;
 	
 	public static final int N = 10;
@@ -12,8 +21,13 @@ public class Processor extends Thread {
 	
 	private int i = 0;
 	
-	public Processor(boolean TSO, MainMemory mMemory){
-		this.mMemory = mMemory;
+	/**
+	 * Processor constructor.
+	 * @param TSO Whether the WriteBuffer is TSO or not.
+	 * @param inputMainMemory The MainMemory that the processor will use.
+	 */
+	public Processor(boolean TSO, MainMemory inputMainMemory){
+		mMemory = inputMainMemory;
 		wBuffer = new WriteBuffer(TSO);
 		mAgent = new MemoryAgent(wBuffer, mMemory);
 	}
